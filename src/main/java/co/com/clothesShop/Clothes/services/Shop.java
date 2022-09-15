b0d09519bfca8177c454636ce375e8414315b3df
Gloria <gloriamendezs13@gmail.com>
2022-09-15 13:53:19 +0200

    Added methods for add, delete and modify products

--- src/main/java/co/com/clothesShop/Clothes/services/Shop.java
+++ src/main/java/co/com/clothesShop/Clothes/services/Shop.java
@@ -1,5 +1,9 @@
 package co.com.clothesShop.Clothes.services;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,5 +28,29 @@ public class Shop {
             if(p.getId() == id)
                 return p;
         return null;
+    }
+
+    public Product addProduct(Product product){
+        product.setId(products.get(products.size()).getId() + 1);
+        this.products.add(product);
+        return product;
+    }
+
+    public Product deleteProduct(int id){
+        Product p = null;
+        for (int i = 0; i < products.size(); i++)
+            if (products.get(i).getId() == id) {
+                p = products.get(i);
+                products.remove(i);
+                break;
+            }
+        return p;
+    }
+
+    public Product modifyProduct(Product product){
+        for (int i = 0; i < products.size(); i++)
+            if (products.get(i).getId() == product.getId())
+                products.set(i, product);
+        return product;
     }
 }
