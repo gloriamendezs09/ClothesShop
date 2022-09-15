6ef82e4eb3b756f5f529cb10d7312ca0048e7b72
Gloria <gloriamendezs13@gmail.com>
2022-09-15 13:51:00 +0200

    Modified controller for product deleting

--- src/main/java/co/com/clothesShop/Clothes/controllers/ControllerDelete.java
+++ src/main/java/co/com/clothesShop/Clothes/controllers/ControllerDelete.java
@@ -1,8 +1,10 @@
 package co.com.clothesShop.Clothes.controllers;
 
+import co.com.clothesShop.Clothes.services.Product;
 import co.com.clothesShop.Clothes.services.Shop;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //Tiene el mismo efecto que la anotación Bean
@@ -14,11 +16,10 @@ public class ControllerDelete {
 
     //Se utiliza para indicar al proyecto cual va a ser la ruta utilizada por el navegador
     //para acceder al método
-    @GetMapping("/delete")
-    public String substract(){
-        int a = 10;
-        int b = 2;
-
-        return ("La resta de los numeros " + a + " - " + b + " es: " + calculator.substract(a, b));
+    @GetMapping("/delete/{id}")
+    public Product delete(@PathVariable ("id") int id){
+        Product p = new Product("Camisa", 10.0);
+        shop.addProduct(p);
+        return shop.deleteProduct(id);
     }
 }
