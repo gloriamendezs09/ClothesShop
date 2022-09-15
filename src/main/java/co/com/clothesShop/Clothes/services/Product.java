7af52cf978ce4f2fb14738ef4cf3f6d20daa6fc9
Gloria <gloriamendezs13@gmail.com>
2022-09-15 12:22:26 +0200

    Class Product created

--- src/main/java/co/com/clothesShop/Clothes/services/Product.java
+++ src/main/java/co/com/clothesShop/Clothes/services/Product.java
@@ -1,2 +1,36 @@
-package co.com.clothesShop.Clothes.services;public class Product {
+package co.com.clothesShop.Clothes.services;
+
+public class Product {
+    int id;
+    String name;
+    Float price;
+
+    public Product(String name, Float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
 }
