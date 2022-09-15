a910506663e7d34dc41b1cdf7f152dc77bffbe4d
Gloria <gloriamendezs13@gmail.com>
2022-09-15 14:37:56 +0200

    Changed GetMapping to PostMapping

--- src/main/java/co/com/clothesShop/Clothes/controllers/ControllerDelete.java
+++ src/main/java/co/com/clothesShop/Clothes/controllers/ControllerDelete.java
@@ -3,9 +3,7 @@ package co.com.clothesShop.Clothes.controllers;
 import co.com.clothesShop.Clothes.services.Product;
 import co.com.clothesShop.Clothes.services.Shop;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //Tiene el mismo efecto que la anotación Bean
 @RestController
@@ -16,8 +14,8 @@ public class ControllerDelete {
 
     //Se utiliza para indicar al proyecto cual va a ser la ruta utilizada por el navegador
     //para acceder al método
-    @GetMapping("/delete/{id}")
-    public Product delete(@PathVariable ("id") int id){
+    @PostMapping("/delete/{id}")
+    public Product delete(@RequestBody int id){
         Product p = new Product("Camisa", 10.0);
         shop.addProduct(p);
         return shop.deleteProduct(id);
